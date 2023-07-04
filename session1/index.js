dd0e7809f1021efb124ceffffc86db8974d328ae
Phuong <zinzin.vung@gmail.com>
2023-07-04 21:26:29 +0700

    update first commit

---
+++ session1/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const PORT = 8000
+const a = 5
+
+app.listen(PORT, () => {
+    console.log("Server is listening on http://localhost:"+PORT)
+})
