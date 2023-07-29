e47053bcb12e3b4841d3f2ba16d1135a0feea1a6
Phuong <zinzin.vung@gmail.com>
2023-07-29 21:32:00 +0700

    update

--- client_project/web69/src/pages/Login/index.jsx
+++ client_project/web69/src/pages/Login/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Button, Checkbox, Form, Input } from 'antd';
 import { login } from '../../services';
-import {useDispatch} from 'react-redux'
-import {Link} from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { Link } from "react-router-dom"
 import HeaderLogin from '../../components/HeaderLogin';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
@@ -24,7 +24,7 @@ const Login = () => {
             toast.success("Đăng nhập thành công")
             dispatch(loginAction(result.data.user))
             saveToLocalStorage("user", JSON.stringify(result.data.user))
-            navigate("/app/dashboard")
+            navigate("/dashboard")
 
         } catch (error) {
             console.log(error)
@@ -35,78 +35,82 @@ const Login = () => {
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
-    
+
 
     return (
-        <div className='' style={{ marginTop: '20px' }}>
-            <HeaderLogin />
-            <Form
-                form={form}
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                style={{
-                    maxWidth: 600,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+        <div className='' style={{ marginTop: '20px', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div>
+                <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Đăng nhập</h1>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
+                <Form
+                    form={form}
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
                     wrapperCol={{
-                        offset: 8,
                         span: 16,
                     }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
+                    style={{
+                        maxWidth: 600,
                     }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Button type="primary" htmlType="submit">
-                        Đăng nhập
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Đăng nhập
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+
         </div>
     );
 }
